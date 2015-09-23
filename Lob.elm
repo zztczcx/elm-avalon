ecba4e64ac96d42f4a3127475b86b0429a9ac56c
chenxu <zztczcx@163.com>
2015-09-23 16:09:00 +0800

    fisrt commit ,try how to make stage

---
+++ Lob.elm
@@ -0,0 +1,19 @@
+module Lob where
+
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (lazy, lazy2, lazy3)
+import Json.Decode as Json
+import Signal exposing (Signal, Address)
+import String
+import Window
+
+---- VIEW ----
+
+view: Address Action -> Model -> Html
+view address model =
+  div 
+    []
+    [ text "lob" ]
